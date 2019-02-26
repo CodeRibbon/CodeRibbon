5c5e6a0284028ccebc4a7d9f3a8299a3d483f887
Ben Klein <robobenklein@gmail.com>
2019-02-26 13:42:05 -0500

    ESlint fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,8 @@ module.exports = {
     "extends": "eslint:recommended",
     "globals": {
         "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+        "SharedArrayBuffer": "readonly",
+        "atom": "readonly"
     },
     "parserOptions": {
         "ecmaVersion": 2018
